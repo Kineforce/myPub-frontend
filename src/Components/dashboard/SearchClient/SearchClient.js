44d8cf1508ce9e39d415b0fd539efc12665aa688
kineforce <lucas-sm2010@hotmail.com>
2021-04-21 16:36:46 -0300

    Ajustes finos

--- src/Components/dashboard/SearchClient/SearchClient.js
+++ src/Components/dashboard/SearchClient/SearchClient.js
@@ -4,18 +4,59 @@ import debounce from "lodash.debounce";
 
 import "./SearchClient.css";
 
-const fetchAll = ({ setClients, setCounterResults }) => {
-  const data = api.get(`/api/clients/all`);
-  data.then((response) => {
-    setClients(response.data);
-    setCounterResults(response.data.length);
-  });
-};
-
 const SearchClient = () => {
   const [searchName, setSearchName] = useState("");
   const [clients, setClients] = useState([]);
   const [counterResults, setCounterResults] = useState(0);
+  const [start, setStart] = useState(0);
+  const [disabled, setDisabled] = useState({
+    left: true,
+    right: false,
+    color: "black",
+  });
+  const [pageIndex, setPageIndex] = useState(0);
+  const resultPerPage = 14;
+
+  let counter = 0;
+
+  function goFoward(e) {
+    if (counter === 14) {
+      setPageIndex(pageIndex + 1);
+      setStart(start + 14);
+      setDisabled({ left: false });
+    } else {
+      setDisabled({ right: true });
+    }
+
+    if (start + resultPerPage + 14 > clients.length) {
+      setDisabled({ right: true });
+    }
+    console.log(`Start          --> ${start}`);
+    console.log(`resultPerPage  --> ${resultPerPage}`);
+    console.log(`clients.length --> ${clients.length}`);
+    console.log(`pageIndex      --> ${pageIndex}`);
+  }
+
+  function goLast() {}
+
+  function goBack(e) {
+    if (start !== 0) {
+      setPageIndex(pageIndex - 1);
+      setStart(start - 14);
+      setDisabled({ right: false });
+    } else {
+      setDisabled({ left: true });
+    }
+
+    if (pageIndex === 1) {
+      setDisabled({ left: true });
+    }
+
+    console.log(`Start          --> ${start}`);
+    console.log(`resultPerPage  --> ${resultPerPage}`);
+    console.log(`clients.length --> ${clients.length}`);
+    console.log(`pageIndex      --> ${pageIndex}`);
+  }
 
   // Executa a ultima chamada da função
   const debouncedSave = useRef(
@@ -24,7 +65,9 @@ const SearchClient = () => {
       var search_counter = 0;
 
       arr_clients.forEach((arr_client) => {
-        if (!arr_client.name.includes(curr_search)) {
+        if (
+          !arr_client.name.toUpperCase().includes(curr_search.toUpperCase())
+        ) {
           arr_client.show_search = 0;
           new_arr.push(arr_client);
         } else {
@@ -41,14 +84,10 @@ const SearchClient = () => {
 
   function handleValue(event) {
     let curr_search = event.target.value;
-    curr_search = curr_search.replace(/[^a-zA-Z. ]/gi, "");
+    curr_search = curr_search.replace(/[^a-zA-Z.0-9 ]/gi, "");
 
     setSearchName(curr_search);
     debouncedSave(curr_search, clients);
-  }
-
-  function fetchAllUsers() {
-    fetchAll({ setClients, setCounterResults });
   }
 
   useEffect(() => {
@@ -80,36 +119,73 @@ const SearchClient = () => {
           <div className="up-side">
             <div className="input-row">
               <input
+                id="search_input"
                 type="text"
                 value={searchName}
                 onChange={handleValue}
-                placeholder="Buscar por nome..."
+                placeholder="Busca dinâmica..."
               ></input>
-              <button
-                style={{ marginLeft: "10px", padding: "12px" }}
-                type="submit"
-                id="buttonAll"
-                onClick={fetchAllUsers}
-              >
-                {" "}
-                Buscar tudo
-              </button>
               <span id="counterResults">
-                Quantidade de resultados: <span>{counterResults}</span>
+                Total de clientes: <span>{counterResults}</span>
               </span>
             </div>
           </div>
           <div className="down-side">
-            {clients.map((client, index) => {
-              if (client.show_search === 1) {
-                return (
-                  <div key={index} className="client-row">
-                    {client.name}
-                  </div>
-                );
-              }
-              return null;
-            })}
+            <table id="search_client_table">
+              <thead>
+                <tr>
+                  <th>Nome </th>
+                  <th>CPF</th>
+                  <th>Gênero</th>
+                  <th>Telefone</th>
+                  <th>Endereço</th>
+                </tr>
+              </thead>
+              {clients.map((client, index) => {
+                if (
+                  client.show_search === 1 &&
+                  index >= start &&
+                  counter < resultPerPage
+                ) {
+                  return (
+                    <>
+                      <tbody {...(counter = counter + 1)}>
+                        <tr
+                          style={{
+                            backgroundColor:
+                              counter % 2 === 0
+                                ? "rgba(75, 75, 75, 0.2)"
+                                : "rgba(0, 0, 0, 0.5)",
+                          }}
+                        >
+                          <td>{client.name}</td>
+                          <td>{client.cpf}</td>
+                          <td>{client.gender}</td>
+                          <td>{client.phone_number}</td>
+                          <td>{client.adress}</td>
+                        </tr>
+                      </tbody>
+                    </>
+                  );
+                }
+                return null;
+              })}
+            </table>
+            <span id="control_page">
+              <button
+                className="fas fa-arrow-left"
+                style={{ color: disabled.color }}
+                onClick={goBack}
+                disabled={disabled.left}
+              ></button>
+              &nbsp;
+              <button
+                className="fas fa-arrow-right"
+                style={{ color: disabled.color }}
+                onClick={goFoward}
+                disabled={disabled.right}
+              ></button>
+            </span>
           </div>
         </div>
       </div>
