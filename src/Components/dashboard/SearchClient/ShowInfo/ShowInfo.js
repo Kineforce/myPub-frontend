1dc3319120863ea2f18a729ce6386451fbb349ff
kineforce <lucas-sm2010@hotmail.com>
2021-04-24 00:22:09 -0300

    Ajustes finos

--- src/Components/dashboard/SearchClient/ShowInfo/ShowInfo.js
+++ src/Components/dashboard/SearchClient/ShowInfo/ShowInfo.js
@@ -1,31 +1,94 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./ShowInfo.css";
+import api from "../../../../api";
 
 const ActionField = () => {
+  const [inputInfo, setInputInfo] = useState("");
+  const [showButton, setShowButton] = useState(false);
+
+  const [feedbackMsg, setFeedbackMsg] = useState({
+    feedbackMsg: "",
+    color: "",
+  });
+
+  function showActionField() {
+    setShowButton(true);
+  }
+
+  function handleValue(event) {
+    setInputInfo(event.target.value);
+  }
+
+  function closeButton() {
+    setShowButton(false);
+  }
+
+  function checkEmpty() {
+    if (inputInfo === "") {
+      setFeedbackMsg({
+        feedbackMsg: "Please, insert some action!",
+        color: "rgba(255, 0, 0, 0.76)",
+      });
+      return;
+    }
+
+    setFeedbackMsg({
+      feedbackMsg: "Action saved!",
+      color: "rgb(204, 78, 204)",
+    });
+  }
+
+  const SaveActionField = () => {
+    return (
+      <div>
+        <i
+          className="fas fa-save saveActionField"
+          onClick={() => checkEmpty()}
+        ></i>
+        <i
+          className="fas fa-times saveActionField"
+          onClick={() => closeButton()}
+        ></i>
+        {feedbackMsg.feedbackMsg && (
+          <span className="saveFeedback" style={{ color: feedbackMsg.color }}>
+            {feedbackMsg.feedbackMsg}
+          </span>
+        )}
+        <input
+          className="actionField"
+          type="text"
+          value={inputInfo}
+          onChange={handleValue}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
-      <input className="actionField" type="text" />
-      <input className="actionField" type="text" />
-      <input className="actionField" type="text" />
-      <input className="actionField" type="text" />
+      {showButton && <SaveActionField />}
+      <button
+        className="btnHist"
+        type="submit"
+        title="Adicionar novo item"
+        onClick={showActionField}
+      >
+        +
+      </button>
     </div>
   );
 };
 
 const ShowInfo = (props) => {
-  const [showButton, setShowButton] = useState(false);
+  const [actions, setActions] = useState([]);
+  let client_id = props.props.client.id;
 
-  function insertAction() {
-    setShowButton(true);
-
-    console.log(showButton);
-    console.log("Send to API to insert action!");
-  }
-
-  function getActions() {
-    console.log("Send to API to get actions!");
-  }
+  useEffect(() => {
+    api.get(`api/clients/addAction/` + client_id).then((response) => {
+      setActions(response.data);
+    });
+  }, [client_id]);
 
   return (
     <>
@@ -53,16 +116,17 @@ const ShowInfo = (props) => {
         </div>
         <div className="clientActions">
           <div className="titleClientActions">Histórico do cliente</div>
-          <div className="dataClientActions">
-            {showButton === true && <ActionField />}
-            <button
-              className="btnHist"
-              type="submit"
-              title="Adicionar novo item"
-              onClick={insertAction}
-            >
-              +
-            </button>
+          <div className="actionDataInfo">
+            {actions.map((action) => {
+              return (
+                <span key={action.id} className="action">
+                  {action.action}
+                </span>
+              );
+            })}
+            <div className="dataClientActions">
+              <ActionField />
+            </div>
           </div>
         </div>
       </div>
